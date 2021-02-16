631bc1631f8b2f8a0c02da021aaf668f229d45ab
Joy Cheung <joycheung1994@gmail.com>
2021-02-16 16:34:53 -0600

    fix __init__

--- qsdsan/_process.py
+++ qsdsan/_process.py
@@ -11,12 +11,16 @@ https://github.com/QSD-Group/QSDsan/blob/master/LICENSE.txt
 for license details.
 '''
 
-import thermosteam as tmo
-from ._parse import get_stoichiometric_coeff
+# import thermosteam as tmo
+from _parse import get_stoichiometric_coeff
 from thermosteam.utils import chemicals_user
 from sympy import symbols, Matrix
+from sympy.parsing.sympy_parser import parse_expr
 import numpy as np
     
+__all__ = ('Process',)
+
+#%%
 @chemicals_user        
 class Process():
     
@@ -24,12 +28,12 @@ class Process():
                  conserved_for=('COD', 'N', 'P', 'charge'), parameters=None):
         
         self._stoichiometry = []
-        self.ref_component = ref_component
         self._components = self._load_chemicals(components)
+        self._ref_component = ref_component
         self._conserved_for = conserved_for
         self._parameters = {p: symbols(p) for p in parameters}
         
-        self._stoichiometry = get_stoichiometric_coeff(reaction, self.ref_component, self._components, self._conserved_for, self._parameters)
+        self._stoichiometry = get_stoichiometric_coeff(reaction, self._ref_component, self._components, self._conserved_for, self._parameters)
         self._parse_rate_eq(rate_equation)
                 
     def get_conversion_factors(self, as_matrix=False):        
@@ -66,10 +70,13 @@ class Process():
             raise RuntimeError("Can only check conservations with numerical "
                                "stoichiometric coefficients.")
     
-    #TODO: what happens to rate eq?
     def reverse(self):
         '''reverse the process as to flip the signs of all components.'''
-        pass
+        if isinstance(self._stoichiometry, np.ndarray):
+            self._stoichiometry = -self._stoichiometry
+        else:
+            self._stoichiometry = [-v for v in self._stoichiometry]
+        self._rate_equation = -self._rate_equation
         
     @property
     def ref_component(self):
@@ -92,25 +99,27 @@ class Process():
     def parameters(self):
         return sorted(self._parameters)
     
-    #TODO: append new symbols to the dictionary
-    def append_parameters(self):
-        pass
+    def append_parameters(self, *new_pars):
+        for p in new_pars:
+            self._parameters[p] = symbols(p)
     
     #TODO: set parameter values (and evaluate coefficients and rate??)
     def set_parameters(self):
         pass
     
+    #!!! only show non-zero coefficients
     @property
     def stoichiometry(self):
-        return dict(zip(self._components, self._stoichiometry))
+        allcmps = dict(zip(self._components.IDs, self._stoichiometry))
+        return {k:v for k,v in allcmps.items() if v != 0}
         
     @property
     def rate_equation(self):
         return self._rate_equation
     
-    #TODO: parse rate equations into symbols
     def _parse_rate_eq(self, eq):
-        pass    
+        cmpconc_symbols = {c: symbols(c) for c in self._components.IDs}
+        self._rate_equation = parse_expr(eq, {**cmpconc_symbols, **self._parameters})
     
     def _normalize_stoichiometry(self, new_ref):
         isa = isinstance
@@ -124,38 +133,3 @@ class Process():
         factor = self._stoichiometry[self._components._index[new_ref]]
         self._rate_equation *= factor
 
-# #%%
-# chemicals = tmo.Chemicals(['H2O', 'H2', 'O2'], cache=True)
-# tmo.settings.set_thermo(chemicals)
-# reaction = tmo.Reaction('H2O -> H2 + 0.5O2', reactant='H2O', X=0.7)
-
-# # from sympy.solvers.solveset import linsolve
-# from sympy import symbols, Matrix, solve
-# # unknowns = symbols('v1:7')
-# f, v1, v2, v3 = symbols('fsi v11 v12 v13')
-# M = Matrix([[1,0,0,1,0,1],[.03, 1, 0, .01, 0, .04], [.01, 0, 1, 0, 0, .01], [0, .07, -.04839, 0, -1, 0]])
-# v = Matrix([1-f, v1, v2, f, v3, -1])
-# sol = solve(M * v, v1, v2, v3)
-
-# # import numpy as np
-# import os
-# os.chdir("C:/Users/joy_c/Dropbox/PhD/Research/QSD/codes_developing/QSD-for-WaSH/sanitation")
-# from qsdsan import Component, Components, WasteStream
-# cmps = Components.load_default()
-# tmo.settings.set_thermo(cmps)
-# # react = cmps.subgroup(('SF', 'SNH4', 'SPO4', 'SU_E', 'SCO3', 'XB_Subst'))
-# # M = Matrix(np.vstack((react.i_COD, react.i_N, react.i_P, react.i_charge)).tolist())
-# p1 = Process('')
-# p1.get_conversion_factors()
-# p1._stoichiometry = np.ones(len(cmps.IDs))
-# p1.check_conservation()
-
-
-# dct = {'XS':-1, 
-#        'SF':'1-fsi',
-#        'SU_E':'fsi',
-#        'SNH4':'?',
-#        'SPO4':'?',
-#        'SCO3':'?'}
-# n_v = sum(v == '?' for v in dct.values())
-# sp.var('v0:%s' % n_v)
