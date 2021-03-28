74eca42498d0ead27d17a075cf2be85713ea0249
Yalin Li <zoe.yalin.li@gmail.com>
2021-03-28 10:07:53 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ QSDsan: Quantitative Sustainable Design for sanitation and resource recovery sys
 This module is developed by:
     Yalin Li <zoe.yalin.li@gmail.com>
 
-This module is under the University of Illinois/NCSA Open Source License. Please refer to
-https://github.com/QSD-Group/QSDsan/blob/master/LICENSE.txt
+This module is under the University of Illinois/NCSA Open Source License.
+Please refer to https://github.com/QSD-Group/QSDsan/blob/main/LICENSE.txt
 for license details.
 '''
 
