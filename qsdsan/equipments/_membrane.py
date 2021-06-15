69bc71141016946112b03ef44cf087d6bfffed7a
Smiti Mittal <59079754+smitimittal@users.noreply.github.com>
2021-06-15 09:59:17 -0700

    Create _membrane.py

---
+++ qsdsan/equipments/_membrane.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+QSDsan: Quantitative Sustainable Design for sanitation and resource recovery systems
+This module is developed by:
+    Smiti Mittal <smitimittal@gmail.com>
+    Yalin Li <zoe.yalin.li@gmail.com>
+    Anna Kogler
+This module is under the University of Illinois/NCSA Open Source License.
+Please refer to https://github.com/QSD-Group/QSDsan/blob/master/LICENSE.txt
+for license details.
+'''
+
+# %%
+import math
+#!!! Change this to relative importing when compiled into qsdsan
+from qsdsan import Equipment, SanUnit, Component, WasteStream
+# from .. import SanUnit, Equipment # relative importing
+
+isinstance = isinstance
+
+# __all__ = ('Electrode', 'ElectroChemCell')
+
+# %%
