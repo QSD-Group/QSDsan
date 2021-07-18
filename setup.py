8ac120e31752ad154286b0717d3d9dae4dd3d7dd
Yalin Li <zoe.yalin.li@gmail.com>
2021-07-18 18:27:16 -0500

    try to fix testing

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     description='Quantitative Sustainable Design for sanitation and resource recovery systems',
     long_description=open('README.rst').read(),
     url="https://github.com/QSD-Group/QSDsan",
-    install_requires=['biosteam==2.30.0', 'thermosteam==0.26.0', 'exposan==0.0.18',
+    install_requires=['biosteam==2.30.0', 'thermosteam==0.26.0',
                       'pandas>=1.2.4', 'matplotlib>=3.3.2', 'scikit-learn', 'scipy',
                       'SALib>=1.4.0b0', 'seaborn', 'sympy'],
     package_data=
