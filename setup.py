41042c3c13559c8bbb9fb82b314d2307eda3a0cc
Yalin <zoe.yalin.li@gmail.com>
2021-10-23 19:11:14 -0500

    update version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 setup(
     name='qsdsan',
     packages=['qsdsan'],
-    version='0.3.8',
+    version='0.3.9',
     license='UIUC',
     author='Quantitative Sustainable Design Group',
     author_email='quantitative.sustainable.design@gmail.com',
