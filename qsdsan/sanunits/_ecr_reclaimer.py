efd47799a4ca02dd0cf9f20e9099bedf1462013f
haclohman <68960449+haclohman@users.noreply.github.com>
2022-04-11 15:06:00 -0500

    user scaling updates
    Cleaned up code and updated costs and LCA to scale with users

--- qsdsan/sanunits/_ecr_reclaimer.py
+++ qsdsan/sanunits/_ecr_reclaimer.py
@@ -1,54 +1,69 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Wed May  5 14:31:47 2021
 
-@author: torimorgan
-"""
+'''
+QSDsan: Quantitative Sustainable Design for sanitation and resource recovery systems
+Copyright (C) 2020, Quantitative Sustainable Design Group
+
+This module is developed by:
+    Tori Morgan <tvlmorgan@gmail.com>
+    Hannah Lohman <hlohman94@gmail.com>
+
+This module is under the University of Illinois/NCSA Open Source License.
+Please refer to https://github.com/QSD-Group/QSDsan/blob/master/LICENSE.txt
+for license details.
+'''
+
+
+import numpy as np
 from qsdsan import SanUnit, Construction
-#from ._decay import Decay
-from qsdsan.utils.loading import load_data, data_path
+from ..utils import ospath, load_data, data_path
+
 
 __all__ = ('ECR_Reclaimer',)
 
-import os
-data_path = os.path.join(data_path, 'sanunit_data/_ECR_Reclaimer.csv')
+ECR_data_path = ospath.join(data_path, 'sanunit_data/_ECR_Reclaimer.csv')
 
-#To scale the system from 0 - 120 users based on units corresponding to 30 users: 
-ppl = 120
-
-if (ppl <=30): R = 1 
-elif (ppl >30 and ppl <=60): R = 2
-elif (ppl >60 and ppl <=90): R = 3
-elif (ppl >90 and ppl <=120): R = 4
-else: R = 5
 
 class ECR_Reclaimer(SanUnit):
     '''
-   Electrochemical treatment with chlorine dosing 
+    Electrochemical treatment with chlorine dosing.
+
+    The following impact items should be pre-constructed for life cycle assessment:
+    Titanium.
 
     Parameters
     ----------
-    ins : WasteStream
-        Waste for treatment, salt. 
-    outs : Treated
-        Treated waste, fugitive CH4, and fugitive N2O.
-   
-        
+    ppl: int
+        Total number of users for scaling of costs.
+    if_gridtied: bool
+        If using grid electricity instead of photovoltaic electricity.
+
+    ins:
+        waste: liquid waste stream to be treated by electrochemical unit
+
+    outs:
+        treated: treated liquid leaving electrochemical unit
+
     References
     ----------
-    .. 2019.06 Technical report for BMGF V3 _ CC 2019.06.13.pdf
-    See Also
-    --------
-    :ref:`qsdsan.sanunits.Decay <sanunits_Decay>`
+    [1] 2019.06 Technical report for BMGF V3 _ CC 2019.06.13.pdf
     
     '''
-    
-    def __init__(self, ID='', ins=None, outs=(), **kwargs):
-        SanUnit.__init__(self, ID, ins, outs, F_BM_default=1)
+
+    # Constants
+    baseline_ppl = 30  # baseline population served by Reclaimer
+    exponent_scale = 0.6  # exponential scaling constant
+
+    def __init__(self, ID='', ins=None, outs=(), thermo=None, init_with='WasteStream', ppl=1, **kwargs):
+        SanUnit.__init__(self, ID, ins, outs, thermo=thermo, init_with=init_with, F_BM_default=1)
         
-        self.price_ratio = 1
-        data = load_data(path=data_path)
+        self.ppl = ppl
+
+        self.qty_reclaimers = np.ceil(self.ppl / self.baseline_ppl)  # number of reclaimer units required
+        self.qty_reclaimers = self.qty_reclaimers.astype(int)  # convert from float to integer
+
+        data = load_data(path=ECR_data_path)
         for para in data.index:
             value = float(data.loc[para]['expected'])
             setattr(self, para, value)
@@ -56,48 +71,46 @@ class ECR_Reclaimer(SanUnit):
         
         for attr, value in kwargs.items():
             setattr(self, attr, value)
-    
-    
+
     _N_ins = 1
     _N_outs = 1
 
-   
     def _run(self):
         waste = self.ins[0]
         treated = self.outs[0]
         treated.copy_like(self.ins[0])
 
-              
-
     def _design(self):
         design = self.design_results
-        design['Titanium'] = electrode_quant = self.Titanium_weight * R
-        self.construction = ((Construction(item='Titanium', quantity = electrode_quant, quantity_unit = 'kg')))
+        design['Titanium'] = electrode_quant = self.Titanium_weight * (self.ppl / self.baseline_ppl)  # linear scale
+        self.construction = Construction(item='Titanium', quantity=electrode_quant, quantity_unit='kg')
         self.add_construction(add_cost=False)
  
     def _cost(self):
-        
-        self.EC_brush_scaled = (self.EC_brush) * R
-        self.EC_cell_scaled = (self.EC_cell) * R
+
         C = self.baseline_purchase_costs
 
-        C['EC_brush'] = self.EC_brush_scaled
-        C['EC_cell'] = self.EC_cell_scaled
+        C['EC_brush'] = self.EC_brush
+        C['EC_cell'] = self.EC_cell
         
-        ratio = self.price_ratio
+        # Exponentially scale capital cost with number of users
+        scale = (self.ppl / self.baseline_ppl) ** self.exponent_scale
         for equipment, cost in C.items():
-            C[equipment] = cost * ratio
-        
-        #If grid
-        self.power_utility(self.power_demand * R / 1000) #kW
-        #If solar
-        # self.power_utility(self.power_demand * 0)
+            C[equipment] = cost * scale
+
+        self.add_OPEX = self._calc_replacement_cost()
+
+        if self.if_gridtied:  # scale linearly with number of units
+            self.power_demand = (self.power_demand_ecr * 24 / 1000) * self.qty_reclaimers  # [W/day][24 hr operation][1 kW/1000 W] = [kWh/d]
+        else:
+            self.power_demand = 0
+
+        self.power_utility(self.power_demand)  # kWh/day
     
     def _calc_replacement_cost(self):
-        ecr_replacement_cost = ((self.EC_cell * (20/self.EC_cell_lifetime)) + 
-        (self.EC_brush * (20/self.EC_brush_lifetime))) * R #USD/yr
-        return ecr_replacement_cost/ (365 * 24) * self.price_ratio # USD/hr (all items are per hour)
-
-        
-       
-        
+        scale = (self.ppl / self.baseline_ppl) ** self.exponent_scale
+        ecr_brush_replacement_cost = scale * (self.EC_brush / self.EC_brush_lifetime)
+        ecr_cell_replacement_cost = scale * (self.EC_cell / self.EC_cell_lifetime)
+        ecr_replacement_cost = ecr_brush_replacement_cost + ecr_cell_replacement_cost
+        ecr_replacement_cost = ecr_replacement_cost / (365 * 24)  # convert from USD/year to USD/hour
+        return ecr_replacement_cost
