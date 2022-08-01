4da7cb4d2a2653f47fa64acdf179eaf0f27f0b46
Yalin Li <mailto.yalin.li@gmail.com>
2022-08-01 15:08:37 -0500

    update `MURT` and links to developed sanitations

--- docs/source/Developed_Systems.rst
+++ docs/source/Developed_Systems.rst
@@ -3,26 +3,30 @@ Developed Systems
 
 This page documents the systems that have been/is being developed using ``QSDsan`` with links to the source codes in GitHub and publications.
 
-+-------------------------+--------------------+--------------------------------+-------------------+
-| System                  | Source Codes       | Publication                    | Module Status     |
-+=========================+====================+================================+===================+
-| Biogenic Refinery       | - Archived `br1`_  | - `Rowles`_ et al., 2022       | Completed         |
-|                         | - Current  `br2`_  |                                |                   |
-+-------------------------+--------------------+--------------------------------+-------------------+
-| BSM1 (benchmark         | - Archived `bsm1`_ | - `Alex`_ et al., 2008         | Completed         |
-| simulation model no. 1) | - Current  `bsm2`_ | - `Li and Zhang`_ et al., 2022 |                   |
-+-------------------------+--------------------+--------------------------------+-------------------+
-| Bwaise                  | - Archived `bw1`_  | - `Trimmer`_ et al., 2020      | Completed         |
-|                         | - Current  `bw2`_  | - `Li and Zhang`_ et al., 2022 |                   |
-+-------------------------+--------------------+--------------------------------+-------------------+
-| CAS (conventional       | - Current  `cas`_  | - `Shoener`_ et al., 2016      | Under development |
-| activated sludge)       |                    |                                |                   |
-+-------------------------+--------------------+--------------------------------+-------------------+
-| NEWgenerator            | - Archived `ng1`_  | - Watabe et al., *In Prep.*    | Under development |
-| (under NDA)             | - Current  `ng2`_  |                                |                   |
-+-------------------------+--------------------+--------------------------------+-------------------+
-| Reclaimer               | - Current   `re`_  | - `Trotochaud`_ et al., 2020   | Under development |
-+-------------------------+--------------------+--------------------------------+-------------------+
++--------------------------+--------------------+--------------------------------+-------------------+
+| System                   | Source Codes       | Publication                    | Module Status     |
++==========================+====================+================================+===================+
+| Biogenic Refinery        | - Archived `br1`_  | - `Rowles`_ et al., 2022       | Completed         |
+|                          | - Current  `br2`_  |                                |                   |
++--------------------------+--------------------+--------------------------------+-------------------+
+| BSM1 (benchmark          | - Archived `bsm1`_ | - `Alex`_ et al., 2008         | Completed         |
+| simulation model no. 1)  | - Current  `bsm2`_ | - `Li and Zhang`_ et al., 2022 |                   |
++--------------------------+--------------------+--------------------------------+-------------------+
+| Bwaise                   | - Archived `bw1`_  | - `Trimmer`_ et al., 2020      | Completed         |
+|                          | - Current  `bw2`_  | - `Li and Zhang`_ et al., 2022 |                   |
++--------------------------+--------------------+--------------------------------+-------------------+
+| CAS (conventional        | - Current  `cas`_  | - `Shoener`_ et al., 2016      | Completed         |
+| activated sludge)        |                    |                                |                   |
++--------------------------+--------------------+--------------------------------+-------------------+
+| Eco-San (under NDA)      | - Current   `es`_  | NA                             | Completed         |
++--------------------------+--------------------+--------------------------------+-------------------+
+| NEWgenerator             | - Archived `ng1`_  | - Watabe et al., *In Prep.*    | Completed         |
+| (under NDA)              | - Current  `ng2`_  |                                |                   |
++--------------------------+--------------------+--------------------------------+-------------------+
+| Reclaimer                | - Current   `re`_  | - `Trotochaud`_ et al., 2020   | Completed         |
++--------------------------+--------------------+--------------------------------+-------------------+
+| SCG Zyclonic (under NDA) | - Current   `sz`_  | NA                             | Completed         |
++--------------------------+--------------------+--------------------------------+-------------------+
 
 Notes:
     - "Under NDA" indicates that the system is under non-disclosure agreement with the technology design team and unfortunately we are not able to share the codes in full at this stage. The source link code will lead to a private repository that only individuals who have signed the NDA can access.
@@ -32,7 +36,7 @@ Notes:
 
 .. Links
 .. _br1: https://github.com/QSD-Group/EXPOsan/tree/BR_OmniProcessor/exposan/OmniProcessor_BiogenicRefinery
-.. _br2: https://github.com/QSD-Group/EXPOsan/tree/gates/exposan/biogenic_refinery
+.. _br2: https://github.com/QSD-Group/EXPOsan/tree/main/exposan/biogenic_refinery
 .. _Rowles: https://chemrxiv.org/engage/chemrxiv/article-details/624ccb31855ee57a9fe327bd
 
 .. _bsm1: https://pypi.org/project/exposan/1.1.4/
@@ -47,8 +51,12 @@ Notes:
 .. _cas: https://github.com/QSD-Group/EXPOsan/tree/main/exposan/cas
 .. _Shoener: https://pubs.rsc.org/en/content/articlelanding/2016/ee/c5ee03715h
 
-.. _ng1: https://github.com/QSD-Group/EXPOsan-private/tree/newgen/exposan/new_generator
-.. _ng2: https://github.com/QSD-Group/EXPOsan-private/tree/gates/exposan/new_generator
+.. _es: https://github.com/QSD-Group/EXPOsan/tree/main/exposan/eco_san
 
-.. _re: https://github.com/QSD-Group/EXPOsan/tree/gates/exposan/reclaimer
+.. _ng1: https://github.com/QSD-Group/EXPOsan-private/tree/newgen/exposan/newgen
+.. _ng2: https://github.com/QSD-Group/EXPOsan-private/tree/main/exposan/new_generator
+
+.. _re: https://github.com/QSD-Group/EXPOsan/tree/main/exposan/reclaimer
 .. _Trotochaud: https://doi.org/10.1021/acs.est.0c02755
+
+.. _sz: https://github.com/QSD-Group/EXPOsan-private/tree/main/exposan/scg_zyclonic
--- qsdsan/sanunits/_toilets.py
+++ qsdsan/sanunits/_toilets.py
@@ -1028,6 +1028,10 @@ class MURT(Toilet):
         waste_out: degraded mixed excreta.
         CH4: fugitive CH4.
         N2O: fugitive N2O.
+    N_squatting_pan_per_toilet : int
+        The number of suqatting pan per toilet.
+    N_urinal_per_toilet : int
+        The number of urinals per toilet.
     if_include_front_end : bool
         If False, will not consider the capital and operating costs of this unit.
 
@@ -1041,6 +1045,7 @@ class MURT(Toilet):
                  N_toilet=None, if_toilet_paper=True, if_flushing=True, if_cleansing=False,
                  if_desiccant=True, if_air_emission=True, if_ideal_emptying=True,
                  CAPEX=0, OPEX_over_CAPEX=0, lifetime=8,
+                 N_squatting_pan_per_toilet=1, N_urinal_per_toilet=1,
                  if_include_front_end=True, **kwargs):
 
         Toilet.__init__(
@@ -1052,9 +1057,9 @@ class MURT(Toilet):
             if_air_emission=if_air_emission, if_ideal_emptying=if_ideal_emptying,
             CAPEX=CAPEX, OPEX_over_CAPEX=OPEX_over_CAPEX
             )
+        self.N_squatting_pan_per_toilet = N_squatting_pan_per_toilet
+        self.N_urinal_per_toilet = N_urinal_per_toilet
         self.if_include_front_end = if_include_front_end
-        #!!! This might be copied from UDDT and should be removed
-        self._tank_V = 60 / 1e3  # m3
         self._mixed = WasteStream(f'{self.ID}_mixed')
 
         data = load_data(path=murt_path)
@@ -1128,7 +1133,10 @@ class MURT(Toilet):
             design['Number of users per toilet'] = self.N_user
             design['Parallel toilets'] = N = self.N_toilet
             design['Collection period'] = self.collection_period
-            design['Ceramic'] = Ceramic_quant = self.squatting_pan_weight + self.urinal_weight
+            design['Ceramic'] = Ceramic_quant = (
+                self.squatting_pan_weight * self.N_squatting_pan_per_toilet+
+                self.urinal_weight * self.N_urinal_per_toilet
+                )
             design['Fan'] = Fan_quant = 1  # assume fan quantity is 1
 
             self.construction = (
@@ -1136,7 +1144,7 @@ class MURT(Toilet):
                 Construction(item='Fan', quantity=Fan_quant * N, quantity_unit='kg'),
             )
 
-            self.add_construction()
+            self.add_construction(add_cost=False)
         else:
             design.clear()
             self.construction = ()
@@ -1145,7 +1153,10 @@ class MURT(Toilet):
         C = self.baseline_purchase_costs
         if self.if_include_front_end:
             N_toilet = self.N_toilet
-            C['Ceramic Toilets'] = (self.squatting_pan_cost + self.urinal_cost) * N_toilet
+            C['Ceramic Toilets'] = (
+                self.squatting_pan_cost * self.N_squatting_pan_per_toilet +
+                self.urinal_cost * self.N_urinal_per_toilet
+                ) * N_toilet
             C['Fan'] = self.fan_cost * N_toilet
             C['Misc. parts'] = (
                 self.led_cost +
@@ -1180,165 +1191,3 @@ class MURT(Toilet):
     @collection_period.setter
     def collection_period(self, i):
         self._collection_period = float(i)
-
-
-# =============================================================================
-# Legacy MURT, not sure where it is used
-# =============================================================================
-
-# murt_path = ospath.join(data_path, 'sanunit_data/_murt.tsv')
-
-# class MURT(Toilet):
-#     '''
-#     Multi-Unit Reinvented Toilet.
-
-#     The following components should be included in system thermo object for simulation:
-#     Tissue, WoodAsh, H2O, NH3, NonNH3, P, K, Mg, CH4, N2O.
-
-#     The following impact items should be pre-constructed for life cycle assessment:
-#     Ceramic, Fan.
-
-#     Parameters
-#     ----------
-#     ins : stream obj
-#         Excreta.
-#     outs : Iterable(stream obj)
-#         Recyclable mixed excreta, stream leached to soil, fugitive CH4, and fugitive N2O.
-#     lifetime : int
-#         Lifetime of this UDDT, [yr].
-#     N_squatting_pans : int
-#         Number of squatting pans.
-#     N_urinal : int
-#         Number of urinals.
-#     N_fan : int
-#         Number of fans.
-
-#     References
-#     ----------
-#     #!!! What should be used for the reference?
-
-#     See Also
-#     --------
-#     :ref:`qsdsan.sanunits.Toilet <sanunits_Toilet>`
-#     '''
-
-#     def __init__(self, ID='', ins=None, outs=(), thermo=None, init_with='WasteStream',
-#                  degraded_components=('OtherSS',), N_user=1, N_toilet=1,
-#                  N_tot_user=None, if_toilet_paper=True, if_flushing=True, if_cleansing=False,
-#                  if_desiccant=False, if_air_emission=True, if_ideal_emptying=True,
-#                  CAPEX=0, OPEX_over_CAPEX=0.075, lifetime=25,
-#                  N_squatting_pan=1, N_urinal=1, N_fan=1,
-#                  **kwargs):
-
-#         Toilet.__init__(self, ID, ins, outs, thermo, init_with,
-#                         degraded_components, N_user, N_toilet, N_tot_user,
-#                         if_toilet_paper, if_flushing, if_cleansing, if_desiccant,
-#                         if_air_emission, if_ideal_emptying, CAPEX, OPEX_over_CAPEX)
-#         self.lifetime = lifetime
-#         #!!! Legacy note: change to be 1 squatting pan and 2 urinals
-#         self.N_squatting_pan = N_squatting_pan
-#         self.N_urinal = N_urinal
-#         self.N_fan = N_fan
-#         self.construction = (
-#             Construction('ceramic', linked_unit=self, item='Cement', quantity_unit='kg'),
-#             Construction('fan', linked_unit=self, item='Sand', quantity_unit='ea'),
-#             )
-
-#         data = load_data(path=data_path)
-#         for para in data.index:
-#             value = float(data.loc[para]['expected'])
-#             setattr(self, para, value)
-#         del data
-
-#         for attr, value in kwargs.items():
-#             setattr(self, attr, value)
-
-#         # Initialize a mixed stream
-#         self._mixed = WasteStream(f'{self.ID}_mixed')
-
-#     _N_outs = 3
-
-#     def _run(self):
-#         Toilet._run(self)
-#         waste, CH4, N2O = self.outs
-#         CH4.phase = N2O.phase = 'g'
-
-#         mixed = self._mixed
-#         mixed.mix_from(self.ins)
-#         tot_COD_kg = sum(float(getattr(i, 'COD'))*i.F_vol for i in self.ins)/1e3
-
-#         # Air emission
-#         if self.if_air_emission:
-#             # N loss due to ammonia volatilization
-#             NH3_rmd, NonNH3_rmd = \
-#                 self.allocate_N_removal(mixed.TN/1e3*mixed.F_vol*self.N_volatilization,
-#                                         mixed.imass['NH3'])
-#             mixed.imass ['NH3'] -= NH3_rmd
-#             mixed.imass['NonNH3'] -= NonNH3_rmd
-#             # Energy/N loss due to degradation
-#             COD_loss = self.first_order_decay(k=self.decay_k_COD,
-#                                               t=self.collection_period/365,
-#                                               max_decay=self.COD_max_decay)
-#             COD_loss_kg = tot_COD_kg * COD_loss
-#             CH4.imass['CH4'] = COD_loss_kg * self.max_CH4_emission * self.MCF_decay
-#             mixed.imass[self.degraded_components] *= 1 - COD_loss
-
-#             N_loss = self.first_order_decay(k=self.decay_k_N,
-#                                             t=self.collection_period/365,
-#                                             max_decay=self.N_max_decay)
-#             N_loss_tot = mixed.TN/1e3*mixed.F_vol*N_loss
-#             NH3_rmd, NonNH3_rmd = \
-#                 self.allocate_N_removal(N_loss_tot,
-#                                         mixed.imass['NH3'])
-#             mixed.imass ['NH3'] -= NH3_rmd
-#             mixed.imass['NonNH3'] -= NonNH3_rmd
-#             N2O.imass['N2O'] = N_loss_tot * self.N2O_EF_decay * 44/28
-#             mixed._COD = (tot_COD_kg-COD_loss_kg)*1e3/mixed.F_vol
-#         else:
-#             CH4.empty()
-#             N2O.empty()
-
-#         # Non-ideal emptying
-#         if not self.if_ideal_emptying:
-#             mixed, CH4, N2O = self.get_emptying_emission(
-#                 waste=mixed, CH4=CH4, N2O=N2O,
-#                 empty_ratio=self.empty_ratio,
-#                 CH4_factor=self.COD_max_decay*self.MCF_aq*self.max_CH4_emission,
-#                 N2O_factor=self.N2O_EF_decay*44/28)
-#         waste.copy_like(mixed)
-#         self._scale_up_outs()
-
-#     _units = {
-#         'Collection period': 'd',
-#         }
-
-#     def _design(self):
-#         design = self.design_results
-#         design['Number of users per toilet'] = self.N_user
-#         design['Parallel toilets'] = N = self.N_toilet
-#         design['Collection period'] = self.collection_period
-#         design['Ceramic (single MURT)'] = Ceramic_quant = \
-#             self.squatting_pan_weight*self.N_squatting_pan + self.urinal_weight*self.N_urinal
-#         design['Fan (single MURT)'] = N_fan = self.N_fan
-
-#         constr = self.construction
-#         constr[0].quantity = Ceramic_quant * N
-#         constr[1].quantity = N_fan * N
-#         self.add_construction()
-
-#     def _cost(self):
-#         C = self.baseline_purchase_costs
-#         N_toilet, N_squatting_pan, N_urinal = self.N_toilet
-#         C['Ceramic Toilets'] = \
-#             (self.squatting_pan_cost*N_squatting_pan+self.urinal_cost*N_urinal)*N_toilet
-#         C['Fan'] = self.fan_cost * N_toilet
-#         C['Misc. parts'] = \
-#             (self.led_cost+self.anticor_floor_cost+self.circuit_change_cost+self.pipe_cost)*N_toilet
-
-#     @property
-#     def collection_period(self):
-#         '''[float] Time interval between storage tank collection, [d].'''
-#         return self._collection_period
-#     @collection_period.setter
-#     def collection_period(self, i):
-#         self._collection_period = float(i)
