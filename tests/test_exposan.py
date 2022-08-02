b8b9da71ab25687843c2e7515d4e49cff79fa057
Yalin Li <mailto.yalin.li@gmail.com>
2022-08-01 22:54:49 -0500

    fix exposan test

--- tests/test_exposan.py
+++ tests/test_exposan.py
@@ -13,14 +13,31 @@ for license details.
 '''
 
 # Just making sure the systems can run is sufficient
+# TODO: change to the new loading routine
 __all__ = ('test_exposan',)
 
 def test_exposan():
+    from exposan import adm
+    # adm.load()
+    adm.sys.simulate(state_reset_hook='reset_cache', t_span=(0, 200), method='BDF')
+
+    from exposan import asm
+    asm1_an = asm.create_asm_validation_system(process_model='ASM1', aerated=False)
+    asm1_aer = asm.create_asm_validation_system(process_model='ASM1', aerated=True)
+    asm2d_an = asm.create_asm_validation_system(process_model='ASM2d', aerated=False)
+    asm2d_aer = asm.create_asm_validation_system(process_model='ASM2d', aerated=True)
+    syses = (asm1_an, asm1_aer, asm2d_an, asm2d_aer)
+    for sys in syses: sys.simulate(state_reset_hook='reset_cache', t_span=(0, 10), method='BDF')
+
     from qsdsan.utils import get_SRT
     from exposan.bsm1 import bsm1
     bsm1.reset_cache()
     bsm1.simulate(t_span=(0,10), method='BDF')
     print(get_SRT(bsm1, biomass_IDs=('X_BH', 'X_BA'))) # to test the `get_SRT` function
+
+    from exposan import biogenic_refinery as br
+    br.load()
+    br.print_summaries((br.sysA, br.sysB, br.sysC, br.sysD))
 
     from exposan import bwaise as bw
     bw.load()
@@ -28,6 +45,10 @@ def test_exposan():
 
     from exposan import cas
     cas.sys.simulate()
+
+    from exposan import reclaimer as re
+    re.load()
+    re.print_summaries((re.sysA, re.sysB, re.sysC, re.sysD))
 
 
 if __name__ == '__main__':
