5d1a0f04ee58dc611bb70f32b9363c201488a4cb
RaiSaumitra <85169426+RaiSaumitra@users.noreply.github.com>
2022-10-04 17:45:53 -0500

    Updated _sluge_treatment
    Added design function for thickener, and created a subclass of thickener 'DewateringUnit'

--- qsdsan/sanunits/_sludge_treatment.py
+++ qsdsan/sanunits/_sludge_treatment.py
@@ -1,36 +1,67 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Sep 30 11:15:25 2022
+'''
+QSDsan: Quantitative Sustainable Design for sanitation and resource recovery systems
 
-@author: raisa
-"""
-from .. import SanUnit, WasteStream
+This module is developed by:
+    
+    Saumitra Rai <raisaumitra9@gmail.com>
+    
+    Joy Zhang <joycheung1994@gmail.com>
+
+This module is under the University of Illinois/NCSA Open Source License.
+Please refer to https://github.com/QSD-Group/QSDsan/blob/main/LICENSE.txt
+for license details.
+'''
+
+from .. import SanUnit
 import numpy as np
 
-__all__ = ('Thickener',)
-
-
+__all__ = ('Thickener', 'DewateringUnit',)
 
 class Thickener(SanUnit):
+    
+    """
+    Thickener based on BSM2 Layout. [1]
+
+    Parameters
+    ----------
+    ID : str
+        ID for the Thickener. The default is ''.
+    ins : class:`WasteStream`
+        Influent to the clarifier. Expected number of influent is 1. 
+    outs : class:`WasteStream`
+        Treated effluent and sludge.
+    thickener_perc : float
+        The percentage of Suspended Sludge in the underflow of the thickener.
+    TSS_removal_perc : float
+        The percentage of suspended solids removed in the thickener. 
+
+    References
+    ----------
+    .. [1] Gernaey, Krist V., Ulf Jeppsson, Peter A. Vanrolleghem, and John B. Copp.
+    Benchmarking of control strategies for wastewater treatment plants. IWA publishing, 2014.
+    """
     
     _N_ins = 1
     _N_outs = 2
     
     def __init__(self, ID='', ins=None, outs=(), thermo=None, isdynamic=False, 
-                  init_with='WasteStream', F_BM_default=None, thickner_perc=7, TSS_removal_perc=98, **kwargs):
-        SanUnit.__init__(self, ID, ins, outs, thermo, isdynamic=isdynamic, init_with=init_with, 
-                         F_BM_default=F_BM_default)
+                  init_with='WasteStream', F_BM_default=None, thickener_perc=7, 
+                  TSS_removal_perc=98, solids_loading_rate = 50, **kwargs):
+        SanUnit.__init__(self, ID, ins, outs, thermo, isdynamic=isdynamic, 
+                         init_with=init_with, F_BM_default=F_BM_default)
         
-        self.thickner_perc = thickner_perc 
+        self.thickener_perc = thickener_perc 
         self.TSS_removal_perc = TSS_removal_perc
+        self.solids_loading_rate = solids_loading_rate 
         
     @property
-    def thickner_perc(self):
+    def thickener_perc(self):
         '''tp is the percentage of Suspended Sludge in the underflow of the thickener'''
         return self._tp
 
-    @thickner_perc.setter
-    def thickner_perc(self, tp):
+    @thickener_perc.setter
+    def thickener_perc(self, tp):
         if tp is not None:
             if tp>=100 or tp<=0:
                 raise ValueError(f'should be between 0 and 100 not {tp}')
@@ -39,8 +70,20 @@ class Thickener(SanUnit):
             raise ValueError('percentage of SS in the underflow of the thickener expected from user')
             
     @property
+    def solids_loading_rate(self):
+        '''solids_loading_rate is the loading in the thickener'''
+        return self._slr
+
+    @solids_loading_rate.setter
+    def solids_loading_rate(self, slr):
+        if slr is not None:
+            self._slr = slr
+        else: 
+            raise ValueError('solids_loading_rate of the thickener expected from user')
+            
+    @property
     def TSS_removal_perc(self):
-        '''The percentage of suspended solids removed in the thickner'''
+        '''The percentage of suspended solids removed in the thickener'''
         return self._TSS_rmv
 
     @TSS_removal_perc.setter
@@ -50,29 +93,29 @@ class Thickener(SanUnit):
                 raise ValueError(f'should be between 0 and 100 not {TSS_rmv}')
             self._TSS_rmv = TSS_rmv
         else: 
-            raise ValueError('percentage of suspended solids removed in the thickner expected from user')
+            raise ValueError('percentage of suspended solids removed in the thickener expected from user')
             
     @property
-    def thickner_factor(self):
+    def thickener_factor(self):
         inf, = self.ins
         if not self.ins: return
         elif inf.isempty(): return
         else: 
             TSS_in = inf.get_TSS()
             if TSS_in > 0:
-                thickner_factor = self._tp*10000/self.ins[0].get_TSS()
-                if thickner_factor<1:
-                    thickner_factor=1
-                return thickner_factor
+                thickener_factor = self._tp*10000/self.ins[0].get_TSS()
+                if thickener_factor<1:
+                    thickener_factor=1
+                return thickener_factor
             else: return None
     
     @property
     def thinning_factor(self):
-        thickner_factor = self.thickner_factor
-        if thickner_factor<1:
+        thickener_factor = self.thickener_factor
+        if thickener_factor<1:
             thinning_factor=0
         else:
-            Qu_factor = self._TSS_rmv/(100*thickner_factor)
+            Qu_factor = self._TSS_rmv/(100*thickener_factor)
             thinning_factor = (1 - (self._TSS_rmv/100))/(1 - Qu_factor)
         return thinning_factor
     
@@ -84,19 +127,60 @@ class Thickener(SanUnit):
         
         TSS_rmv = self._TSS_rmv
         thinning_factor = self.thinning_factor
-        thickner_factor = self.thickner_factor
+        thickener_factor = self.thickener_factor
         
-        Ze = (1 - thinning_factor)/(thickner_factor - thinning_factor)*inf.mass*cmps.s
-        Zs = (thickner_factor - 1)/(thickner_factor - thinning_factor)*inf.mass*cmps.s
+        Ze = (1 - thinning_factor)/(thickener_factor - thinning_factor)*inf.mass*cmps.s
+        Zs = (thickener_factor - 1)/(thickener_factor - thinning_factor)*inf.mass*cmps.s
         
         Xe = (1 - TSS_rmv/100)*inf.mass*cmps.x
         Xs = (TSS_rmv/100)*inf.mass*cmps.x
         
         Ce = Ze + Xe 
         Cs = Zs + Xs
-        
+    
         of.set_flow(Ce,'kg/hr')
         uf.set_flow(Cs,'kg/hr')
         
-        #inf.cmps.s.split_to(uf, of, split= (1 - thinning_factor)/(thickner_factor - thinning_factor))
-        #inf.cmps.x.split_to(uf, of, split = TSS_rmv)
+    def _design(self):
+        
+        design = self.design_results
+        slr = self._slr
+                
+        design['Area'] = ((self.ins[0].get_TSS()/1000)*self.ins[0].F_vol*24)/slr # in m2
+        design['Hydraulic_Loading'] = (self.ins[0].F_vol*24)/design['Area'] #in m3/(m2*day)
+        design['Diameter'] = np.sqrt(4*design['Area']/np.pi) #in m
+            
+class DewateringUnit(Thickener):
+    
+    """
+    Dewatering Unit based on BSM2 Layout. [1]
+    
+    Parameters
+    ----------
+    ID : str
+        ID for the Dewatering Unit. The default is ''.
+    ins : class:`WasteStream`
+        Influent to the Dewatering Unit. Expected number of influent is 1. 
+    outs : class:`WasteStream`
+        Treated effluent and sludge.
+    thickener_perc : float
+        The percentage of Suspended Sludge in the underflow of the dewatering unit.
+    TSS_removal_perc : float
+        The percentage of suspended solids removed in the dewatering unit. 
+    
+    References
+    ----------
+    .. [1] Gernaey, Krist V., Ulf Jeppsson, Peter A. Vanrolleghem, and John B. Copp.
+    Benchmarking of control strategies for wastewater treatment plants. IWA publishing, 2014.
+    """
+    
+    _N_ins = 1
+    _N_outs = 2
+    
+    def __init__(self, ID='', ins=None, outs=(), thermo=None, isdynamic=False, 
+                  init_with='WasteStream', F_BM_default=None, thickener_perc=28, TSS_removal_perc=98, **kwargs):
+        Thickener.__init__(self, ID=ID, ins=ins, outs=outs, thermo=thermo, isdynamic=isdynamic,
+                      init_with=init_with, F_BM_default=F_BM_default, thickener_perc=thickener_perc, TSS_removal_perc=TSS_removal_perc, **kwargs)
+        
+    def _design(self):
+        pass
