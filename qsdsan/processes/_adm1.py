2c4f66c3d43b400c34bf58c0336f19bd1d34de1d
Joy Zhang <joycheung1994@gmail.com>
2023-01-08 18:33:09 -0600

    Debug ASM/ADM interface models

--- qsdsan/processes/_adm1.py
+++ qsdsan/processes/_adm1.py
@@ -278,9 +278,12 @@ def rhos_adm1(state_arr, params):
     if S_va > 0: rhos[7] *= 1/(1+S_bu/S_va)
     if S_bu > 0: rhos[8] *= 1/(1+S_va/S_bu)
 
-    h = brenth(acid_base_rxn, 1e-14, 1.0,
-               args=(weak_acids, Kas),
-               xtol=1e-12, maxiter=100)
+    try: 
+        h = brenth(acid_base_rxn, 1e-14, 1.0,
+                args=(weak_acids, Kas),
+                xtol=1e-12, maxiter=100)
+    except: breakpoint()
+    # h = 10**(-7.46)
 
     nh3 = Kas[1] * weak_acids[2] / (Kas[1] + h)
     co2 = weak_acids[3] - Kas[2] * weak_acids[3] / (Kas[2] + h)
