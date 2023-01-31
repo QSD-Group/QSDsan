dd7af425983bab22453ab2346260523009ef8163
Yalin Li <mailto.yalin.li@gmail.com>
2023-01-31 16:28:02 -0600

    update versions

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 setup(
     name='qsdsan',
     packages=['qsdsan'],
-    version='1.2.3',
+    version='1.2.4',
     license='UIUC',
     author='Quantitative Sustainable Design Group',
     author_email='quantitative.sustainable.design@gmail.com',
