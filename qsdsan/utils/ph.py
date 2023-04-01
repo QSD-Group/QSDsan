1c9668032db0276c33a82a20792b4a038c2ab57a
Jianan Feng <jiananf2@illinois.edu>
2023-03-31 20:03:20 -0500

    modify precipitation; add a test file

--- qsdsan/utils/ph.py
+++ qsdsan/utils/ph.py
@@ -16,7 +16,6 @@ __all__ = ('pH_solver',)
 def pH_solver(kw=10**-14,
               activity=True,
               check_precipitation = True,
-              precise = 2,
               weak_chemicals={},
               other_chemicals={}):
     '''
@@ -73,12 +72,152 @@ def pH_solver(kw=10**-14,
             for i in range(len(chemical)-1):
                 eqn_list.append(sym.Eq(eqn_dict[chemical[i+1][0]]*H - chemicals[chemical][i]*eqn_dict[chemical[i][0]],0))
 
-    pH = iterator(eqn_list, eqn_dict, chemicals, kw=kw, precipitation=check_precipitation)
-    for i in range(precise+1):
-        pH = iterator(eqn_list, eqn_dict, chemicals,*pH[:2], kw=kw, precipitation=check_precipitation)
+    pH = iterator(eqn_list, eqn_dict, chemicals, kw=kw)
+    for i in range(3):
+        pH = iterator(eqn_list, eqn_dict, chemicals,*pH[:2], kw=kw)
+        
+    pH_value = min(round(pH[0], 2), round(pH[1], 2))
     
-    while abs((pH[2]-pH[3])/pH[2]) > 0.01 or abs((pH[2]-pH[3])/pH[3]) > 0.01:
-        pH = iterator(eqn_list, eqn_dict, chemicals,*pH[:2], kw=kw, precipitation=check_precipitation)
+    
+        
+        
+        
+        
+        
+        
+        
+        
+    if check_precipitation:   
+        # get all ions   
+        ions = {}
+        for item in pH[2].items():
+            ions[str(item[0])] =item[1]
+        ions['OH'] = kw/ions['H']
+        
+        # get ion:charge dict
+        chemical_ion = {}
+        for chemical in chemicals.keys():
+            for i in chemical:
+                chemical_ion[i[0]] = i[1]
+        chemical_ion['H'] = 1
+        chemical_ion['OH'] = -1
+    
+        # separate ions to cations and anions and also remove neutral ones
+        cations = {}
+        anions = {}
+        for ion in ions.items():
+            if chemical_ion[ion[0]] > 0:
+                cations[ion[0]] = ion[1]
+            else:
+                anions[ion[0]] = ion[1]
+    
+    
+    
+        eqn_list=[sym.Eq(sym.symbols('OH')*sym.symbols('H'),10**-14),] # change to kw
+        eqn_dict={
+            'H': sym.symbols('H'),
+            'OH': sym.symbols('OH'),
+                  }
+        
+        # identify possible precipitations:
+        for cation in cations.items():
+            if cation[0] in list(precipitation_inventory.keys()):
+                for anion in anions.items():
+                    if anion[0] in precipitation_inventory[cation[0]]:
+                        index = precipitation_inventory[cation[0]].index(anion[0])
+                        ksp = precipitation_inventory[cation[0]][index+1]
+                        
+                        least_common = lcm(abs(chemical_ion[cation[0]]), abs(chemical_ion[anion[0]]))
+                        cation_cof = int(least_common/abs(chemical_ion[cation[0]]))
+                        anion_cof = int(least_common/abs(chemical_ion[anion[0]]))
+                            
+
+                        
+                        if (ions[cation[0]]**cation_cof)*(ions[anion[0]]**anion_cof) > ksp:
+                            # add new equations here?
+                            eqn_list.append(sym.Eq((sym.symbols(cation[0])**cation_cof)*(sym.symbols(anion[0])**anion_cof),ksp))
+                            eqn_dict[f'({cation[0]}){cation_cof}({anion[0]}){anion_cof}'] = sym.symbols(f'({cation[0]}){cation_cof}({anion[0]}){anion_cof}')
+                            
+                            # substract from the MB:
+                            for chemical in chemicals.items():
+                                if cation[0] in list(dict(chemical[0]).keys()):
+                                    new_tot = chemicals[chemical[0]][-1] - cation_cof*sym.symbols(f'({cation[0]}){cation_cof}({anion[0]}){anion_cof}')
+                                    tempo_list = list(chemicals[chemical[0]])
+                                    tempo_list[-1] = new_tot
+                                    tempo_dict = {chemical[0]: tuple(tempo_list)}
+                                    chemicals.update(tempo_dict)
+                            
+                                if anion[0] in list(dict(chemical[0]).keys()):
+                                    new_tot = chemicals[chemical[0]][-1] - anion_cof*sym.symbols(f'({cation[0]}){cation_cof}({anion[0]}){anion_cof}')
+                                    tempo_list = list(chemicals[chemical[0]])
+                                    tempo_list[-1] = new_tot
+                                    tempo_dict = {chemical[0]: tuple(tempo_list)}
+                                    chemicals.update(tempo_dict)
+                            
+                            
+                            
+# !!! forget activitiy for precipitation for now
+
+        for chemical in list(chemicals):
+            for i in range(len(chemical)):
+                eqn_dict[chemical[i][0]] = sym.symbols(f'{chemical[i][0]}')
+                
+        # mass balance
+                try:
+                    MB+=eqn_dict[chemical[i][0]]
+                except NameError:
+                    MB=eqn_dict[chemical[i][0]]
+            eqn_list.append(sym.Eq(MB, chemicals[chemical][-1]))
+            MB-=MB
+
+        # equilibrium
+            if len(chemical) > 1:
+                for i in range(len(chemical)-1):
+                    eqn_list.append(sym.Eq(eqn_dict[chemical[i+1][0]]*H - chemicals[chemical][i]*eqn_dict[chemical[i][0]],0))
+
+# !!! add a charge balance equation
+
+        
+                            
+        pH = iterator(eqn_list, eqn_dict, chemicals, kw=kw)
+        for i in range(3):
+            pH = iterator(eqn_list, eqn_dict, chemicals,*pH[:2], kw=kw)
+            
+        pH_value = min(round(pH[0], 2), round(pH[1], 2))     
+        
+        
+        
+        
+        
+        eqn_list.append(sym.Eq(H, 10**(-pH_value)))
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+
 
     if activity == True:
         ions_sym = pH[4]
@@ -101,7 +240,7 @@ def pH_solver(kw=10**-14,
     
     return pH, ions
     
-def iterator(eqn_list=None, eqn_dict=None, chemicals={}, minimum=-10, maximum=24, kw=None, precipitation=None):
+def iterator(eqn_list=None, eqn_dict=None, chemicals={}, minimum=-10, maximum=24, kw=None):
     
     step = max(maximum-minimum+1, 11)
     # step = 11
@@ -111,6 +250,9 @@ def iterator(eqn_list=None, eqn_dict=None, chemicals={}, minimum=-10, maximum=24
         eqn_list.append(sym.Eq(H,10**(-potential_pH)))
         
         eqn_dict['H'] = H
+        
+        
+        # !!! directly add a charge balance instead of attempting pH
 
         ans = sym.solve(eqn_list,tuple(eqn_dict.values()), dict=True)
         
@@ -124,14 +266,24 @@ def iterator(eqn_list=None, eqn_dict=None, chemicals={}, minimum=-10, maximum=24
                     if chemical[i][0] == str(list(ans.keys())[j]):
                         charge += chemical[i][1]*float(ans[list(ans.keys())[j]])
                         I += 0.5*float(ans[list(ans.keys())[j]])*(chemical[i][1]**2)
-        del eqn_list[-1]
+        
         charge_dict[potential_pH] = (charge, I, ans)
+        del eqn_list[-1]
         
         if len(charge_dict) > 1:
             charge_values = list(charge_dict.values())
             if charge_values[-1][0]*charge_values[-2][0] <= 0:
                 break
             
+    return list(charge_dict.keys())[-2], list(charge_dict.keys())[-1], ans
+
+    
+    
+    
+    
+    
+    
+    # move the below part to pH_solver
     if precipitation:   
         # get all ions   
         ions = {}
@@ -158,8 +310,11 @@ def iterator(eqn_list=None, eqn_dict=None, chemicals={}, minimum=-10, maximum=24
     
     
     
-        eqn_list=[]
-        eqn_dict={}
+        eqn_list=[sym.Eq(sym.symbols('OH')*sym.symbols('H'),10**-14),] # change to kw
+        eqn_dict={
+            'H': sym.symbols('H'),
+            'OH': sym.symbols('OH'),
+                  }
         
         # identify possible precipitations:
         for cation in cations.items():
@@ -178,6 +333,7 @@ def iterator(eqn_list=None, eqn_dict=None, chemicals={}, minimum=-10, maximum=24
                         if (ions[cation[0]]**cation_cof)*(ions[anion[0]]**anion_cof) > ksp:
                             # add new equations here?
                             eqn_list.append(sym.Eq((sym.symbols(cation[0])**cation_cof)*(sym.symbols(anion[0])**anion_cof),ksp))
+                            eqn_dict[f'({cation[0]}){cation_cof}({anion[0]}){anion_cof}'] = sym.symbols(f'({cation[0]}){cation_cof}({anion[0]}){anion_cof}')
                             
                             # substract from the MB:
                             for chemical in chemicals.items():
@@ -218,10 +374,6 @@ def iterator(eqn_list=None, eqn_dict=None, chemicals={}, minimum=-10, maximum=24
 
                             
                             
-                            
-                            
-                        
-                        print(cation[0]+anion[0]+str(precipitation_inventory[cation[0]][index+1]))
                     
             
             
---
+++ qsdsan/utils/ph_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 31 08:30:34 2023
+
+@author: jiananfeng
+"""
+
+from qsdsan.utils.ph import pH_solver
+
+pH_solver(precise=0, activity=True,
+          weak_chemicals={'PO4':0.05, 'NH3':0.05, 'arsenic':0.05, 'HF':0.05, 'lactic':0.05, 'ascorbic': 0.05, 'acetate':0.05, 'propionic':0.05, 'SO4':0.05},
+          other_chemicals={(('Cl', -1),): (0.05,), (('Mg', 2),): (0.05,)})
+
+chemicals={(('H3PO4', 0), ('H2PO4', -1), ('HPO4', -2), ('PO4', -3)): (0.00752, 6.230000000000001e-08, 4.8e-13, 0.05), (('NH4', 1), ('NH3', 0)): (5.6e-10, 0.05), (('H3AsO4', 0), ('H2AsO4', -1), ('HAsO4', -2), ('AsO4', -3)): (0.005, 8e-08, 6e-10, 0.05), (('HF', 0), ('F', -1)): (0.00072, 0.05), (('HCH3H5O3', 0), ('CH3H5O3', -1)): (0.000138, 0.05), (('H2C6H6O6', 0), ('HC6H6O6', -1), ('C6H6O6', -2)): (7.900000000000001e-05, 1.6e-12, 0.05), (('CH3COOH', 0), ('CH3COO', -1)): (1.76e-05, 0.05), (('CH3CH2COOH', 0), ('CH3CH2COO', -1)): (1.3400000000000002e-05, 0.05), (('HSO4', -1), ('SO4', -2)): (0.012, 0.05), (('Cl', -1),): (0.05,), (('Mg', 2),): (0.05,)}
