928c6d4176db7f00aaf3a416d96c343c50fc8b37
JunhyungParkenv <145532861+JunhyungParkenv@users.noreply.github.com>
2024-03-25 22:52:16 -0500

    Back to Previous Condition

--- qsdsan/processes/ADM1_vfa_pH_231030.py
+++ qsdsan/processes/ADM1_vfa_pH_231030.py
@@ -120,16 +120,16 @@ default_init_conds = {
     'X_ch': 10*1e3,
     'X_pr': 10*1e3,
     'X_li': 10*1e3,
-    'X_su': 10*1e3,
-    'X_aa': 10*1e3,
-    'X_fa': 10*1e3,
-    'X_la': 0.1*1e2,
-    'X_et': 0.1*1e2,
+    'X_su': 0.5*1e3,
+    'X_aa': 0.5*1e3,
+    'X_fa': 0.5*1e3,
+    'X_la': 0.1*1e1,
+    'X_et': 0.1*1e1,
     'X_c4': 0.1*1e2,
     'X_pro': 0.1*1e2,
-    'X_ac': 10*1e5,
-    'X_h2': 10*1e3,
-    'X_I': 10*1e3
+    'X_ac': 1.0*1e3,
+    'X_h2': 1.0*1e3,
+    'X_I': 1.0*1e3
     }                   # in mg/L
 
 U1.set_init_conc(**default_init_conds)                          # set initial condition of AD
