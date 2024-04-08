899423fb81371ab1cb2483f37a9c90d85f5e39d2
JunhyungParkenv <145532861+JunhyungParkenv@users.noreply.github.com>
2024-04-08 04:00:42 -0500

    .

--- qsdsan/processes/_adm1_vfa.py
+++ qsdsan/processes/_adm1_vfa.py
@@ -690,7 +690,7 @@ class ADM1_vfa(CompiledProcesses):
                 Y_su=0.1, Y_aa=0.08, Y_fa=0.06, Y_la=0.06, Y_et=0.04, Y_c4=0.06, Y_pro=0.04, Y_ac=0.05, Y_h2=0.06,
                 q_dis=0.05, q_ch_hyd=5.0, q_pr_hyd=5.0, q_li_hyd=5.0,
                 k_su=30, k_aa=50, k_fa=6, k_la=1.0, k_et=6.41, k_c4=20, k_pro=5.5, k_ac=4.0, k_h2=35,
-                K_su=0.5, K_aa=0.3, K_fa=0.4, K_la=0.01, K_et=0.91, K_c4=0.357, K_pro=0.392, K_ac=0.01, K_h2=7e-6,
+                K_su=0.5, K_aa=0.3, K_fa=0.4, K_la=0.8, K_et=0.91, K_c4=0.357, K_pro=0.392, K_ac=0.01, K_h2=7e-6,
                 b_su=0.02, b_aa=0.02, b_fa=0.02, b_la=0.02, b_et=0.02, b_c4=0.02, b_pro=0.02, b_ac=0.02, b_h2=0.02,
                 KI_h2_fa=5e-6, KI_h2_c4=1e-5, KI_h2_pro=3.5e-6, KI_h2_la=3e-4, KI_h2_et=1e-5, KI_nh3=1.8e-3, KS_IN=1e-4,
                 KI_la_ac=12, KI_ac=0.01,
