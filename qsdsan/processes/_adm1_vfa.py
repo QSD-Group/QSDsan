1e466f79462bda797b9215398a3805da88304a85
JunhyungParkenv <145532861+JunhyungParkenv@users.noreply.github.com>
2024-04-18 01:18:45 -0500

    .

--- qsdsan/processes/_adm1_vfa.py
+++ qsdsan/processes/_adm1_vfa.py
@@ -683,7 +683,7 @@ class ADM1_vfa(CompiledProcesses):
     #f_la_su, f_et_su, f_pro_la, f_ac_la,  f_ac_et, Y_la, Y_et added with valued randomly below
     def __new__(cls, components=None, path=None, N_xc=2.686e-3, N_I=4.286e-3, N_aa=7e-3,
                 f_ch_xc=0.6, f_pr_xc=0.1, f_li_xc=0.3, f_xI_xc=0.3,
-                f_fa_li=0.01, f_la_su=0.433, f_et_su= 0.1, f_bu_su=0.05,
+                f_fa_li=0.01, f_la_su=0.533, f_et_su= 0.1, f_bu_su=0.05,
                 f_ac_su=0.02, f_va_aa=0.33, f_bu_aa=0.17, f_pro_aa=0.17, f_ac_aa=0.16,
                 f_ac_fa=0.6, f_pro_la=0.4, f_ac_la=0.12, f_ac_et=0.42, f_pro_va=0.64,
                 f_ac_va=0.12, f_ac_bu=0.12, f_ac_pro=0.12, f_pro_h2=0.15,
