bef8afdd7d31f53bd0215235fd2ed36de19d6412
Joy Zhang <joycheung1994@gmail.com>
2024-05-30 14:34:21 -0700

    minor fix of doctest

--- qsdsan/sanunits/_suspended_growth_bioreactor.py
+++ qsdsan/sanunits/_suspended_growth_bioreactor.py
@@ -773,10 +773,11 @@ class PFR(SanUnit):
     >>> inf = WasteStream('inf', H2O=1.53e6, S_I=46, S_S=54, X_I=1770, X_S=230, 
     ...                   X_BH=3870, X_BA=225, X_P=680, S_O=0.377, S_NO=7.98, 
     ...                   S_NH=25.6, S_ND=5.87, X_ND=13.4, S_ALK=103)
-    >>> AS = su.PFR('AS', ins=(inf,), outs=('eff',), V_tanks=[1000]*2+[1333]*3, 
-    ...         influent_fractions=[[1.0, 0,0,0,0]], DO_setpoints=[0]*2+[1.7, 2.4, 0.5],
-    ...         internal_recycles=[(4,0,55338)], DO_ID='S_O',
-    ...         suspended_growth_model=asm1)
+    >>> AS = su.PFR('AS', ins=(inf,), outs=('eff',), 
+    ...             N_tanks_in_series=5, V_tanks=[1000]*2+[1333]*3, 
+    ...             influent_fractions=[[1.0, 0,0,0,0]], DO_setpoints=[0]*2+[1.7, 2.4, 0.5],
+    ...             internal_recycles=[(4,0,55338)], DO_ID='S_O',
+    ...             suspended_growth_model=asm1)
     >>> AS.set_init_conc(S_I=30, S_S=5, X_I=1000, X_S=100, X_BH=500, X_BA=100, 
     ...                  X_P=100, S_O=2, S_NO=20, S_NH=2, S_ND=1, X_ND=1, S_ALK=84)
     >>> AS.simulate(t_span=(0,100), method='BDF')
