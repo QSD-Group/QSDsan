5cc97d397539f0bed7f11c7720827671036e169b
JunhyungParkenv <145532861+JunhyungParkenv@users.noreply.github.com>
2024-10-02 22:51:12 -0500

    .

--- qsdsan/CEE598_HW1.py
+++ qsdsan/CEE598_HW1.py
@@ -8,15 +8,15 @@ Created on Wed Oct  2 21:03:25 2024
 import pandas as pd
 
 # 파일 경로 설정 (파일 경로는 자신의 경로로 설정해야 함)
-faostat_file_path = 'FAOSTAT1.xlsx'
-vwc_file_path = 'vwc.xlsx'
-
+faostat_file_path = 'process_data/FAOSTAT1.xlsx'
+vwc_file_path = 'process_data/VWC.xlsx'
+#%%
 # FAOSTAT 데이터 불러오기 (CSV 파일)
 faostat_data = pd.read_excel(faostat_file_path)
-
+#%%
 # VWC 데이터 불러오기 (Excel 파일)
 vwc_data = pd.read_excel(vwc_file_path)
-
+#%%
 # 필요한 열만 사용하여 VWC 데이터 정리 (필요한 경우 열 이름 변경)
 vwc_data_cleaned = vwc_data.rename(columns={'Product description (HS)': 'Item', 'vwc': 'VWC'})
 
