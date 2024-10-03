17bb73dcbdc013331f68dd2cc3e200a223075424
JunhyungParkenv <145532861+JunhyungParkenv@users.noreply.github.com>
2024-10-02 22:41:44 -0500

    .

--- qsdsan/CEE598_HW1.py
+++ qsdsan/CEE598_HW1.py
@@ -8,8 +8,8 @@ Created on Wed Oct  2 21:03:25 2024
 import pandas as pd
 
 # 파일 경로 설정 (파일 경로는 자신의 경로로 설정해야 함)
-faostat_file_path = 'FAOSTAT1.csv'
-vwc_file_path = 'vwc.csv'
+faostat_file_path = 'FAOSTAT1.xlsx'
+vwc_file_path = 'vwc.xlsx'
 
 # FAOSTAT 데이터 불러오기 (CSV 파일)
 faostat_data = pd.read_csv(faostat_file_path)
