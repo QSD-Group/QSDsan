2ca94ce29bca42c3b3c7274701e8cfc74efe5526
JunhyungParkenv <145532861+JunhyungParkenv@users.noreply.github.com>
2024-10-04 00:58:11 -0500

    .

--- qsdsan/CEE598_HW1.py
+++ qsdsan/CEE598_HW1.py
@@ -6,17 +6,24 @@ Created on Wed Oct  2 21:03:25 2024
 """
 
 import pandas as pd
-
+import matplotlib.pyplot as plt
 # 파일 경로 설정
-faostat_file_1 = 'FAOSTAT1.xlsx'
-faostat_file_2 = 'FAOSTAT2.xlsx'
-faostat_file_3 = 'FAOSTAT3.xlsx'
+faostat_file_1 = 'FAO1.xlsx'
+faostat_file_2 = 'FAO2.xlsx'
+faostat_file_3 = 'FAO3.xlsx'
+faostat_file_4 = 'FAO4.xlsx'
+faostat_file_5 = 'FAO5.xlsx'
+faostat_file_6 = 'FAO6.xlsx'
+
 vwc_file_path = 'VWC.xlsx'
 
 # FAOSTAT 데이터 불러오기
 faostat_data_1 = pd.read_excel(faostat_file_1)
 faostat_data_2 = pd.read_excel(faostat_file_2)
 faostat_data_3 = pd.read_excel(faostat_file_3)
+faostat_data_4 = pd.read_excel(faostat_file_4)
+faostat_data_5 = pd.read_excel(faostat_file_5)
+faostat_data_6 = pd.read_excel(faostat_file_6)
 
 # VWC 데이터 불러오기
 vwc_data = pd.read_excel(vwc_file_path)
@@ -38,3 +45,15 @@ print(merged_data.head())
 
 # 최종 데이터 CSV 파일로 저장하기 (원하는 경우)
 merged_data.to_csv('output_combined_vwt_data.csv', index=False)
+#%%
+# 피벗 테이블 생성 및 저장
+export_import_matrix = pd.pivot_table(merged_data, values='VWT', 
+                                      index='Exporting Countries', 
+                                      columns='Importing Countries', 
+                                      aggfunc='sum', fill_value=0)
+
+# 피벗 테이블 CSV 파일로 저장
+export_import_matrix.to_csv('export_import_matrix.csv')
+
+# 피벗 테이블의 첫 몇 행 출력
+print(export_import_matrix.head())
