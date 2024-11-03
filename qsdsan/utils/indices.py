95f17c3801cb96de2e8c28a3235f317e356b0202
Yalin <mailto.yalin.li@gmail.com>
2024-11-03 07:18:32 -0500

    add TEA-related indices

--- qsdsan/utils/indices.py
+++ qsdsan/utils/indices.py
@@ -7,6 +7,8 @@ This module is developed by:
     
     Yalin Li <mailto.yalin.li@gmail.com>
 
+    Ali Ahmad <aa3056@scarletmail.rutgers.edu>
+
 This module is under the University of Illinois/NCSA Open Source License.
 Please refer to https://github.com/QSD-Group/QSDsan/blob/main/LICENSE.txt
 for license details.
