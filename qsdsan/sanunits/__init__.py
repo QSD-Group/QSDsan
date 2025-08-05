54511c9ce95ffa9c3b7ae299dde367081112b45a
Joy Zhang <joycheung1994@gmail.com>
2025-08-05 14:17:20 -0700

    moved tech-specific units to EXPOsan-private
    per NDA

--- qsdsan/sanunits/__init__.py
+++ qsdsan/sanunits/__init__.py
@@ -85,14 +85,10 @@ from ._septic_tank import *
 from ._toilet import *
 from ._treatment_bed import *
 
-# System-specific unit (public)
+# System-specific unit
 from ._biogenic_refinery import *
 from ._eco_san import *
 from ._reclaimer import *
-
-# System-specific unit (private)
-from ._new_generator import *
-from ._scg_zyclonic import *
 
 # From then on the order doesn't matter, listed alphabetically
 from . import (
@@ -137,9 +133,6 @@ from . import (
         _biogenic_refinery,
         _eco_san,
         _reclaimer,
-        # System-specific units (private)
-        _new_generator,
-        _scg_zyclonic,
         _sludge_treatment,
         )
 
@@ -181,13 +174,10 @@ __all__ = (
         *_treatment_bed.__all__,
         *_trucking.__all__,
 
-        # System-specific units (public)
+        # System-specific units
         *_biogenic_refinery.__all__,
         *_reclaimer.__all__,
         *_eco_san.__all__,
-        # System-specific units (private)
-        *_new_generator.__all__,
-        *_scg_zyclonic.__all__,
         *_sludge_treatment.__all__,
         *_membrane_gas_extraction.__all__,
         )
