affd78a7141516ff046f258a25eb6c8e6deee5c6
Yalin Li <mailto.yalin.li@gmail.com>
2026-07-02 10:52:37 -0400

    consolidate Excretion unit

--- qsdsan/unit_operations/static/_g2rt.py
+++ qsdsan/unit_operations/static/_g2rt.py
@@ -23,6 +23,7 @@ import numpy as np
 import CoolProp.CoolProp as CP
 from ..bst import Mixer, IsothermalCompressor
 from ... import SanUnit, Construction, WasteStream
+from ._excretion import Excretion
 from ._non_reactive import Copier
 from ...utils import ospath, data_path, load_data, price_ratio
 
@@ -840,304 +841,27 @@ class FWMixer(Mixer):
         # print(f"The flushing water flow is {flushing.imass['H2O']} kg/h.")
 
 #%%
-excretion_path = ospath.join(data_path, 'sanunit_data/_excretion.tsv')
-class G2RTExcretion(SanUnit):
+class G2RTExcretion(Excretion):
     '''
-    Estimation of N, P, K, and COD in urine and feces based on dietary intake
-    for one person based on `Trimmer et al. <https://doi.org/10.1021/acs.est.0c03296>`_
+    G2RT-specific excretion model. Thin subclass of :class:`~.Excretion` that
+    represents feces/urine COD as explicit soluble/particulate component mass
+    flows (`sCOD`/`xCOD`) instead of a lumped `_COD` concentration override,
+    since G2RT's downstream separation units need component-level COD to
+    model soluble vs. particulate removal. Uses the same underlying data
+    (`_excretion.tsv`) and all other assumptions as :class:`~.Excretion`.
 
-    Parameters
-    ----------
-    waste_ratio : float
-        A ratio in [0, 1] to indicate the amount of intake calories and nutrients
-        (N, P, K) that is wasted.
-
-    Examples
+    See Also
     --------
-    `bwaise systems <https://github.com/QSD-Group/EXPOsan/blob/main/exposan/bwaise/systems.py>`_
-
-    References
-    ----------
-    [1] Trimmer et al., Navigating Multidimensional Social–Ecological System
-    Trade-Offs across Sanitation Alternatives in an Urban Informal Settlement.
-    Environ. Sci. Technol. 2020, 54 (19), 12641–12653.
-    https://doi.org/10.1021/acs.est.0c03296.
+    :class:`~.Excretion`
     '''
 
-    _N_ins = 0
-    _N_outs = 2
-
-    def __init__(self, ID='', ins=None, outs=(), thermo=None, init_with='WasteStream',
-                 waste_ratio=0, **kwargs):
-        SanUnit.__init__(self, ID, ins, outs, thermo, init_with)
-        self.waste_ratio = waste_ratio
-
-        data = load_data(path=excretion_path)
-        for para in data.index:
-            value = float(data.loc[para]['expected'])
-            setattr(self, '_'+para, value)
-        del data
-
-        for attr, value in kwargs.items():
-            setattr(self, attr, value)
-
-    def _run(self):
-        ur, fec = self.outs
-        ur.empty()
-        fec.empty()
-        not_wasted = 1 - self.waste_ratio
-        factor = 24 * 1e3 # from g per person per day to kg per hour
-
-        ur_N = (self.p_veg+self.p_anim)/factor*self.N_prot \
-           * self.N_exc*self.N_ur*not_wasted
-        ur.imass['NH3'] = ur_N * self.N_ur_NH3
-        ur.imass['NonNH3'] = ur_N - ur.imass['NH3']
-
-        ur.imass['P'] = (self.p_veg*self.P_prot_v+self.p_anim*self.P_prot_a)/factor \
-            * self.P_exc*self.P_ur*not_wasted
-
-        e_cal = self.e_cal / 24 * not_wasted
-        ur.imass['K'] = e_cal/1e3 * self.K_cal/1e3 * self.K_exc*self.K_ur
-        ur.imass['Mg'] = self.Mg_ur / factor
-        ur.imass['Ca'] = self.Ca_ur / factor
-
-        ur_exc = self.ur_exc / factor
-        ur.imass['H2O'] = self.ur_moi * ur_exc
-        ur.imass['OtherSS'] = ur_exc - ur.F_mass
-
-        fec_exc = self.fec_exc / factor
-        fec_N = (1-self.N_ur)/self.N_ur * ur_N
-        fec.imass['NH3'] = fec_N * self.N_fec_NH3
-        fec.imass['NonNH3'] = fec_N - fec.imass['NH3']
-        fec.imass['P'] = (1-self.P_ur)/self.P_ur * ur.imass['P']
-        fec.imass['K'] = (1-self.K_ur)/self.K_ur * ur.imass['K']
-        fec.imass['Mg'] = self.Mg_fec / factor
-        fec.imass['Ca'] = self.Ca_fec / factor
-        fec.imass['H2O'] = self.fec_moi * fec_exc
-        fec.imass['OtherSS'] = fec_exc - fec.F_mass
-
-        # 14 kJ/g COD, the average lower heating value of excreta
-        tot_COD = e_cal*self.e_exc*4.184/14/1e3 # in kg COD/hr
-        ur.imass['sCOD'] = tot_COD*(1-self.e_fec) # in kg/hr
-        fec.imass['xCOD'] = tot_COD*self.e_fec # in kg/hr
-        
     @property
     def power_kW(self):
         return 0.0 #kW
 
-    @property
-    def e_cal(self):
-        '''[float] Caloric intake, [kcal/cap/d].'''
-        return self._e_cal
-    @e_cal.setter
-    def e_cal(self, i):
-        self._e_cal = i
-
-    @property
-    def p_veg(self):
-        '''[float] Vegetal protein intake, [g/cap/d].'''
-        return self._p_veg
-    @p_veg.setter
-    def p_veg(self, i):
-        self._p_veg = i
-
-    @property
-    def p_anim(self):
-        '''[float] Animal protein intake, [g/cap/d].'''
-        return self._p_anim
-    @p_anim.setter
-    def p_anim(self, i):
-        self._p_anim = i
-
-    @property
-    def N_prot(self):
-        '''[float] Nitrogen content in protein, [wt%].'''
-        return self._N_prot
-    @N_prot.setter
-    def N_prot(self, i):
-        self._N_prot = i
-
-    @property
-    def P_prot_v(self):
-        '''[float] Phosphorus content in vegetal protein, [wt%].'''
-        return self._P_prot_v
-    @P_prot_v.setter
-    def P_prot_v(self, i):
-        self._P_prot_v = i
-
-    @property
-    def P_prot_a(self):
-        '''[float] Phosphorus content in animal protein, [wt%].'''
-        return self._P_prot_a
-    @P_prot_a.setter
-    def P_prot_a(self, i):
-        self._P_prot_a = i
-
-    @property
-    def K_cal(self):
-        '''[float] Potassium intake relative to caloric intake, [g K/1000 kcal].'''
-        return self._K_cal
-    @K_cal.setter
-    def K_cal(self, i):
-        self._K_cal = i
-
-    @property
-    def N_exc(self):
-        '''[float] Nitrogen excretion factor, [% of intake].'''
-        return self._N_exc
-    @N_exc.setter
-    def N_exc(self, i):
-        self._N_exc = i
-
-    @property
-    def P_exc(self):
-        '''[float] Phosphorus excretion factor, [% of intake].'''
-        return self._P_exc
-    @P_exc.setter
-    def P_exc(self, i):
-        self._P_exc = i
-
-    @property
-    def K_exc(self):
-        '''[float] Potassium excretion factor, [% of intake].'''
-        return self._K_exc
-    @K_exc.setter
-    def K_exc(self, i):
-        self._K_exc = i
-
-    @property
-    def e_exc(self):
-        '''[float] Energy excretion factor, [% of intake].'''
-        return self._e_exc
-    @e_exc.setter
-    def e_exc(self, i):
-        self._e_exc = i
-
-    @property
-    def N_ur(self):
-        '''[float] Nitrogen recovered in urine, [wt%].'''
-        return self._N_ur
-    @N_ur.setter
-    def N_ur(self, i):
-        self._N_ur = i
-
-    @property
-    def P_ur(self):
-        '''[float] Phosphorus recovered in urine, [wt%].'''
-        return self._P_ur
-    @P_ur.setter
-    def P_ur(self, i):
-        self._P_ur = i
-
-    @property
-    def K_ur(self):
-        '''[float] Potassium recovered in urine, [wt%].'''
-        return self._K_ur
-    @K_ur.setter
-    def K_ur(self, i):
-        self._K_ur = i
-
-    @property
-    def e_fec(self):
-        '''[float] Percent of excreted energy in feces, [%].'''
-        return self._e_fec
-    @e_fec.setter
-    def e_fec(self, i):
-        self._e_fec = i
-
-    @property
-    def N_ur_NH3(self):
-        '''[float] Reduced inorganic nitrogen in urine, modeled as NH3, [% of total urine N].'''
-        return self._N_ur_NH3
-    @N_ur_NH3.setter
-    def N_ur_NH3(self, i):
-        self._N_ur_NH3 = i
-
-    @property
-    def N_fec_NH3(self):
-        '''[float] Reduced inorganic nitrogen in feces, modeled as NH3, [% of total feces N].'''
-        return self._N_fec_NH3
-    @N_fec_NH3.setter
-    def N_fec_NH3(self, i):
-        self._N_fec_NH3 = i
-
-    @property
-    def ur_exc(self):
-        '''[float] Urine generated per day, [g/cap/d].'''
-        return self._ur_exc
-    @ur_exc.setter
-    def ur_exc(self, i):
-        self._ur_exc = i
-
-    @property
-    def fec_exc(self):
-        '''[float] Feces generated per day, [g/cap/d].'''
-        return self._fec_exc
-    @fec_exc.setter
-    def fec_exc(self, i):
-        self._fec_exc = i
-
-    @property
-    def ur_moi(self):
-        '''[float] Moisture (water) content of urine, [wt%].'''
-        return self._ur_moi
-    @ur_moi.setter
-    def ur_moi(self, i):
-        self._ur_moi = i
-
-    @property
-    def fec_moi(self):
-        '''[float] Moisture (water) content of feces, [wt%].'''
-        return self._fec_moi
-    @fec_moi.setter
-    def fec_moi(self, i):
-        self._fec_moi = i
-
-    @property
-    def Mg_ur(self):
-        '''[float] Magnesium excreted in urine, [g Mg/cap/d].'''
-        return self._Mg_ur
-    @Mg_ur.setter
-    def Mg_ur(self, i):
-        self._Mg_ur = i
-
-    @property
-    def Mg_fec(self):
-        '''[float] Magnesium excreted in feces, [g Mg/cap/d].'''
-        return self._Mg_fec
-    @Mg_fec.setter
-    def Mg_fec(self, i):
-        self._Mg_fec = i
-
-    @property
-    def Ca_ur(self):
-        '''[float] Calcium excreted in urine, [g Ca/cap/d].'''
-        return self._Ca_ur
-    @Ca_ur.setter
-    def Ca_ur(self, i):
-        self._Ca_ur = i
-
-    @property
-    def Ca_fec(self):
-        '''[float] Calcium excreted in feces, [g Ca/cap/d].'''
-        return self._Ca_fec
-    @Ca_fec.setter
-    def Ca_fec(self, i):
-        self._Ca_fec = i
-
-    @property
-    def waste_ratio(self):
-        '''
-        [float] The amount of intake calories and nutrients
-        (N, P, K) that is wasted.
-
-        .. note::
-            Not considered for Mg and Ca.
-        '''
-        return self._waste_ratio
-    @waste_ratio.setter
-    def waste_ratio(self, i):
-        self._waste_ratio = i
+    def _set_COD(self, ur, fec, tot_COD):
+        ur.imass['sCOD'] = tot_COD*(1-self.e_fec) # in kg/hr
+        fec.imass['xCOD'] = tot_COD*self.e_fec # in kg/hr
 
 #%%
 vr_pasteurization_path = ospath.join(g2rt_su_data_path, '_vr_pasteurization.csv')
